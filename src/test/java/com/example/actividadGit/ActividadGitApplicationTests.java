4c86161e7c8056fac72b3d4aa3fb0364a8bc7c25
joaquin <jo.ayalah@duocuc.cl>
2026-04-01 17:47:52 -0300

    mi primer commit

---
+++ src/test/java/com/example/actividadGit/ActividadGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.actividadGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActividadGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
